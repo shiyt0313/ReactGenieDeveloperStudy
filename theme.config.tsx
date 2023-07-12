6cd19f2dc8357553daa7764fe0e9f8137b384d40
shiyt <shiyingtian_thu@163.com>
2023-07-12 11:58:36 -0700

    Update basic info

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <><span>ReactGenie</span><text> test the text/</text></>  ,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   footer: {
     text: 'Nextra Docs Template',
   },
