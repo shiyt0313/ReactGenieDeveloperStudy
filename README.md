8b048cf6d58872876f4ea87c99d993b66cf26569
shiyt0313 <shiyt0313@gmail.com>
2023-07-14 17:08:28 -0700

    Update docs

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ReactGenie for Developer
 
+Access the [website](https://react-genie-developer-study.vercel.app/geniedoc/decorators) for more details.
 ## License
 
 This project is licensed under the MIT License.
--- theme.config.tsx
+++ theme.config.tsx
@@ -6,9 +6,9 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   footer: {
     text: 'ReactGenie for Developer',
